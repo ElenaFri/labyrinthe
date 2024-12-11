91d03f0d3c67b5c1496b9e951d21df15c429a6fb
Chettati Yasmine <yasmine.chettati@etu.unistra.fr>
2024-12-11 18:10:00 +0100

    main dans labyrinth

--- src/main/labyrinth/Labyrinth.java
+++ src/main/labyrinth/Labyrinth.java
@@ -1,7 +1,30 @@
-package labyrinth;
+package main.labyrinth;
 
-public class Labyrinth {
+import main.labyrinth.models.data.ImageStore;
+import main.labyrinth.models.game.GameFacade;
+import main.labyrinth.models.game.Gameboard;
+import main.labyrinth.views.ViewsForObservers.GameBoardFacadeView;
+
+import javax.swing.*;
+
+public  class Labyrinth {
+
 	public static void main(String[] args) {
+		// Initialisation du modèle et des composants nécessaires
+		Gameboard gameboard = new Gameboard();
+		GameFacade gameFacade = new GameFacade();
+		ImageStore imageStore = new ImageStore();
 
+		// Créez l'interface graphique
+		JFrame frame = new JFrame("Labyrinth Game");
+		GameBoardFacadeView view = new GameBoardFacadeView(gameboard, gameFacade, imageStore);
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(view);
+		frame.pack();
+		frame.setSize(1920, 1080);
+		frame.setLocationRelativeTo(null); // Centrer la fenêtre
+		frame.setVisible(true);
 	}
 }
+
--- src/main/labyrinth/views/ViewsForObservers/GameBoardFacadeView.java
+++ src/main/labyrinth/views/ViewsForObservers/GameBoardFacadeView.java
@@ -559,21 +559,5 @@ public class GameBoardFacadeView extends JPanel implements GameBoardObserver, Ga
             repaint();  // Redessiner le panneau après la rotation
         }
     }
-    public static void main(String[] args) {
-        // Initialisation du modèle et des composants nécessaires
-        Gameboard gameboard = new Gameboard();
-        GameFacade gameFacade = new GameFacade();
-        ImageStore imageStore = new ImageStore();
 
-        // Créez l'interface graphique
-        JFrame frame = new JFrame("Labyrinth Game");
-        GameBoardFacadeView view = new GameBoardFacadeView(gameboard, gameFacade, imageStore);
-
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(view);
-        frame.pack();
-        frame.setSize(1920, 1080);
-        frame.setLocationRelativeTo(null); // Centrer la fenêtre
-        frame.setVisible(true);
-    }
 }
